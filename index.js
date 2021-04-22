e8ad02a5cf632e2cb88a6a97df4916577dd88a3c
Adnan Hebibovic <adnan.hebibovic@gmail.com>
2021-04-22 09:45:35 +0200

    Small config changes

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
 app.listen(PORT, function () {
   console.log('Server is up and running on port ' + PORT);
 });
