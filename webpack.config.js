9b788f3da94a6a612dfeb2f08871a32ab7595b91
Adnan Hebibovic <adnan.hebibovic@gmail.com>
2021-04-22 09:38:26 +0200

    Small config changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,48 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './src/app.js',
-    output: {
+  plugins: [new MiniCssExtractPlugin({filename: 'styles.css'})],
+  entry: './src/app.js',
+  output: {
         path: path.resolve(__dirname, 'public/src'),
         filename: 'bundle.js',
-        publicPath: "/"
-      },
-    mode: 'development',
-    module: {
-      rules: [{
-          test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env','@babel/preset-react'],
-              plugins: [
-                require('@babel/plugin-proposal-object-rest-spread'),
+        publicPath: "/",
+        clean: true,
+  },
+  mode: 'production',
+  module: {
+    rules: [{
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env','@babel/preset-react'],
+            plugins: [
+              require('@babel/plugin-proposal-object-rest-spread'),
                 require('@babel/plugin-transform-spread')
-              ]
-            }
+            ]
           }
+      }
+      }, {
+        test: /\.s?css$/,
+        use: [{
+          loader: 'style-loader'
         }, {
-          test: /\.s?css$/,
-          use: [{
-            loader: 'style-loader'
-          }, {
-            loader: 'css-loader'
-          }, {
-            loader: 'sass-loader'
-          }]
+          loader: 'css-loader'
+        }, {
+          loader: 'sass-loader'
         }]
-    },
-    devtool: 'source-map',
-    devServer: {
-      contentBase: path.resolve(__dirname, 'public'),
-      publicPath: '/src',
-      historyApiFallback: true,
-      hot: true
-    }
+      }, {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }]
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    publicPath: '/src',
+    historyApiFallback: true
+  }
 }
